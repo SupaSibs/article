6592de8453306ee69374c61a596b6b1c0aaf573a
SupaSibs <omareeto2012@hotmail.com>
2022-02-09 19:29:58 +0200

    s

---
+++ src/components/FEM.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function FEM() {
+return (
+<span>
+<p>Made using <a href="frontendmentor.io">Frontend Mentor</a></p>
+</span>
+)
+}
---
+++ src/components/Main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+export default function Main(props: any) {
+return (<>
+<main>
+
+<div>
+<img src="../images/avatar-michelle.jpg" alt="of Michelle"/>
+</div>
+
+  <div id="one">
+	<h1>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+<p>Ever been in a room and felt like something was missing?
+Perhaps it felt slightly bare and uninviting.
+I've got some simple tips to help you make any room feel complete.</p>
+	<div>
+	  <footer><span><p>Michelle Appleton</p><p>28 Jun 2020</p></span><button onClick={openShareable}>Share</button></footer>
+	</div>
+  </div>
+</main>
+</>)
+}
+
+
+function openShareable() {
+
+}
